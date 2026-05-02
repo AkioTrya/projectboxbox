a97305a343db7ae09ea94e9959abce35ff1131d7
Cio <akiotryacharisma@gmail.com>
2026-05-02 14:51:29 +0700

    minor bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description: "F1 Telemetry Dashboard",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
